92bdd3ca7a4e00d88c22f04035b225c95b90b547
Akshit Malhotra <aks.m.18@gmail.com>
2015-01-23 17:21:11 +0100

    init

---
+++ examples/simple.js
@@ -0,0 +1,3 @@
+// if you don't specify a html file, the sniper will generate a div
+var app = require("biojs-vis-globemap");
+var instance = new app({el: yourDiv, worldMapJSON: '../data/world-countries.json', markerJSON: "../data/markerEvents.json"});
