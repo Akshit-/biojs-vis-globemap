1afa4a57d20f00e1388f3ed07a96465878bfaa34
Seb <greenify@users.noreply.github.com>
2015-01-30 14:19:14 +0100

    instance renamed to biojs-instance

--- examples/simple.js
+++ examples/simple.js
@@ -1,5 +1,3 @@
 var app = require("biojs-vis-globemap");
 var instance = new app({el: yourDiv, worldMapJSON: '../data/world-countries.json', markerJSON: "../data/markerEvents.json"});
-
-// Annotation for the BioJS event system test (you can safely remove this in your app)
-//instance=instance
+//@biojs-instance=instance - Annotation for the BioJS event system test (you can safely remove this in your app)
