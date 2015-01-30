91bd814fe8cd43548a765ea43c2041cdc517bb91
Sebastian Wilzbach <seb@wilzba.ch>
2015-01-30 12:42:51 +0100

    send events to other components

--- examples/simple.js
+++ examples/simple.js
@@ -1,3 +1,5 @@
-// if you don't specify a html file, the sniper will generate a div
 var app = require("biojs-vis-globemap");
 var instance = new app({el: yourDiv, worldMapJSON: '../data/world-countries.json', markerJSON: "../data/markerEvents.json"});
+
+// Annotation for the BioJS event system test (you can safely remove this in your app)
+//instance=instance
